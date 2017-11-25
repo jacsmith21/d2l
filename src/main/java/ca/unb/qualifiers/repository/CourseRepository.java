707d12253d1eef952048de521c5b3708da22ff43
alexodonn <alex.odonnell@unb.ca>
2017-11-25 11:39:47 -0400

    Added fonts

--- src/main/java/ca/unb/qualifiers/repository/CourseRepository.java
+++ src/main/java/ca/unb/qualifiers/repository/CourseRepository.java
@@ -4,5 +4,9 @@ import ca.unb.qualifiers.model.Course;
 import ca.unb.qualifiers.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends CrudRepository<Course, Integer> {
+
+    List<Course> findByInstructorUsernameAndNameAndSection(String username, String name, String section);
 }
