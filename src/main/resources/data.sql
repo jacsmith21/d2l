3f1d10a4b5bb7c5766d750f96500755ed64e6806
Jacob Smith <jacob.smith@unb.ca>
2017-11-25 11:12:07 -0400

    fixed db bug

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT IGNORE INTO user (id, username, email, password, instructor)  VALUES (1, 'student', 'example@example.com', '$2a$10$5fh0xrsnizwp5WKqSO1kYeMPIeulnjUV.mBsgOGg7hrGoT9ZphcMW', FALSE );
