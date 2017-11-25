09f20f6da0589f75ec9f500859fad136bb37ecbf
Brandon Richardson <brandon1024.br@gmail.com>
2017-11-25 13:31:05 -0400

    Began course view

--- src/main/java/ca/unb/qualifiers/controller/CourseController.java
+++ src/main/java/ca/unb/qualifiers/controller/CourseController.java
@@ -7,8 +7,13 @@ import ca.unb.qualifiers.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+import java.util.List;
 
 @Controller
 @EnableAutoConfiguration
@@ -20,5 +25,17 @@ public class CourseController {
     @ResponseBody
     public Iterable<Course> getCourses() {
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{username}/{coursename}")
+    public String dashboard(ModelMap map, Principal principal, @PathVariable String username, @PathVariable String coursename) {
+        if (!principal.getName().equals(username)) {
+            return "404";
+        }
+
+        Course course = courseRepository.findByName(coursename);
+
+
+        return "course";
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,3 @@
-SET @@global.sql_mode= 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
-
 INSERT IGNORE INTO user (id, username, email, password, instructor)  VALUES (1, 'student', 'example@example.com', '$2a$10$5fh0xrsnizwp5WKqSO1kYeMPIeulnjUV.mBsgOGg7hrGoT9ZphcMW', FALSE );
 INSERT IGNORE INTO user (id, username, email, password, instructor)  VALUES (2, 'instructor', 'example@example.com', '$2a$10$5fh0xrsnizwp5WKqSO1kYeMPIeulnjUV.mBsgOGg7hrGoT9ZphcMW', FALSE );
 INSERT IGNORE INTO course (id, name, section, instructor_id) VALUES(1, 'ENGG1003', 'FR01A', 2);
