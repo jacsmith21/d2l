9da0008aa4881cb6a7ef58aedd478d4148ddd521
Jacob Smith <jacob.smith@unb.ca>
2017-11-25 10:11:51 -0400

    fixed config

--- src/main/java/ca/unb/qualifiers/config/UrlHandlerMappingConfig.java
+++ src/main/java/ca/unb/qualifiers/config/UrlHandlerMappingConfig.java
@@ -1,9 +1,11 @@
 package ca.unb.qualifiers.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+@Configuration
 public class UrlHandlerMappingConfig extends WebMvcConfigurationSupport {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
