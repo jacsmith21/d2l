c601117c24fb358fb1664c21b519d44a6b077454
Brandon Richardson <brandon1024.br@gmail.com>
2017-11-25 14:56:21 -0400

    can download files now

--- src/main/java/ca/unb/qualifiers/controller/CourseController.java
+++ src/main/java/ca/unb/qualifiers/controller/CourseController.java
@@ -2,9 +2,11 @@ package ca.unb.qualifiers.controller;
 
 import ca.unb.qualifiers.model.Course;
 import ca.unb.qualifiers.model.Deliverable;
+import ca.unb.qualifiers.model.Upload;
 import ca.unb.qualifiers.model.User;
 import ca.unb.qualifiers.repository.CourseRepository;
 import ca.unb.qualifiers.repository.DeliverableRepository;
+import ca.unb.qualifiers.repository.UploadRepository;
 import ca.unb.qualifiers.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
+import java.util.LinkedList;
 import java.util.List;
 
 @Controller
@@ -25,6 +28,12 @@ public class CourseController {
 
     @Autowired
     DeliverableRepository deliverableRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UploadRepository uploadRepository;
 
     @GetMapping("/courses")
     @ResponseBody
@@ -38,9 +47,19 @@ public class CourseController {
             return "404";
         }
 
+        User user = userRepository.findByUsername(username);
         Course course = courseRepository.findByName(coursename);
         List<Deliverable> deliverables = deliverableRepository.findByCourse(course);
+
         map.addAttribute("deliverables", deliverables);
+        map.addAttribute("user", user);
+
+        if(user.isInstructor()) {
+            List<Upload> uploads = new LinkedList<>();
+            for(Deliverable deliverable : deliverables)
+                uploads.addAll(deliverable.getUploads());
+            map.addAttribute("uploads", uploads);
+        }
 
         return "course";
     }
